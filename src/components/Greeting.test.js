0520e8ec5a21c09d635af0e0aa7e5097b9d2458a
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-20 19:17:08 +0200

    use "query" to test for missing elements

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -11,7 +11,7 @@ describe("Greeting component", () => {
     expect(helloWorldElement).toBeInTheDocument();
   });
 
-  test("renders good to see you if the button was NOT clicked", () => {
+  test("renders 'good to see you' if the button was NOT clicked", () => {
     render(<Greeting />);
 
     const outputElement = screen.getByText("good to see you", { exact: false });
@@ -27,5 +27,17 @@ describe("Greeting component", () => {
 
     const outputElement = screen.getByText("Changed!");
     expect(outputElement).toBeInTheDocument();
+  });
+
+  test("does not render 'good to see you' if the button WAS clicked", () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    const outputElement = screen.queryByText("good to see you", {
+      exact: false,
+    });
+    expect(outputElement).toBeNull();
   });
 });
