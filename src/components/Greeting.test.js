0590ff834a15cc2f7e3be0642fc3ac97e385d115
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2021-05-20 19:09:18 +0200

    test user interaction and state changes

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 describe("Greeting component", () => {
@@ -6,6 +7,25 @@ describe("Greeting component", () => {
     render(<Greeting />);
 
     const helloWorldElement = screen.getByText("Hello World!");
+
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders good to see you if the button was NOT clicked", () => {
+    render(<Greeting />);
+
+    const outputElement = screen.getByText("good to see you", { exact: false });
+
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("renders 'Changed!' if the button was clicked", () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    const outputElement = screen.getByText("Changed!");
+    expect(outputElement).toBeInTheDocument();
   });
 });
